6ba64fe1ad0bf5502616027a70c08232061b7cc4
anastasia_cher <anastasiia.cher@yandex.ru>
2024-09-23 18:28:13 +0300

    Delete homework05 directory

--- homework05/vkapi/config.py
+++
@@ -1,7 +0,0 @@
-# https://hackernoon.com/4-ways-to-manage-the-configuration-in-python-4623049e841b
-
-VK_CONFIG = {
-    "domain": "https://api.vk.com/method",
-    "access_token": "",
-    "version": "5.199",
-}
