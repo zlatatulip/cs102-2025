b8d757eea6b9fce2913f520ddc833d342545187a
zlatatulip <zlatatulip@gmail.com>
2025-11-16 19:46:27 +0300

    Update caesar.py

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -11,7 +11,21 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    s = [range(ord('A'), ord('Z') + 1), range(ord('a'), ord('z') + 1), range(ord('А'), ord('Я') + 1), range(ord('а'), ord('я') + 1)]
+    for i in plaintext:
+        t1 = ord(i)
+        t2 = ord(i) + shift
+        flag = 0
+        for j in s:
+            if t1 in j:
+                while t2 not in j:
+                    t2 -= len(j)
+                flag = 1
+                break
+        if flag:
+            ciphertext += chr(t2)
+        else:
+            ciphertext += i
     return ciphertext
 
 
@@ -28,5 +42,19 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    s = [range(ord('A'), ord('Z') + 1), range(ord('a'), ord('z') + 1), range(ord('А'), ord('Я') + 1), range(ord('а'), ord('я') + 1)]
+    for i in ciphertext:
+        t1 = ord(i)
+        t2 = ord(i) - shift
+        flag = 0
+        for j in s:
+            if t1 in j:
+                while t2 not in j:
+                    t2 += len(j)
+                flag = 1
+                break
+        if flag:
+            plaintext += chr(t2)
+        else:
+            plaintext += i
     return plaintext
