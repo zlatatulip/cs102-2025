f49e69b0c6773e6055356480d5b4bfc10d27013e
zlatatulip <zlatatulip@gmail.com>
2025-11-18 14:19:07 +0300

    Update affine.py

--- homework01/affine.py
+++ homework01/affine.py
@@ -1,17 +1,14 @@
-def encrypt_affine(plaintext: str, a: int, b: int, m: int) -> str:
-    """
-    Encrypts plaintext using a Vigenere cipher.
-    >>> encrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> encrypt_vigenere("python", "a")
-    'python'
-    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
-    'LXFOPVEFRNHR'
-    """
+def encrypt_affine(plaintext: str, a: int, b: int) -> str:
     ciphertext = ""
     for i in plaintext:
         if i.isalpha():
-            ciphertext += chr((a * ord(i) + b) % m)
+            if i.isupper():
+                t = ord("A")
+            else:
+                t = ord("a")
+            ciphertext += chr(t + (a * ord(i) + b) % 26)
+        else:
+            ciphertext += i
     return ciphertext
 
 
@@ -20,6 +17,6 @@ if __name__ == "__main__":
     p = int(input("Enter a number: "))
     q = int(input("Enter another number: "))
     message = input("Enter a message to encrypt: ")
-    encrypted_msg = encrypt_affine(message, p, q, 26)
+    encrypted_msg = encrypt_affine(message, p, q)
     print("Your encrypted message is: ")
     print(encrypted_msg)
