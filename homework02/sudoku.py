75bb16e69d2aa9d73e52d5cf518a111750a9796e
zlatatulip <zlatatulip@gmail.com>
2025-11-30 02:36:47 +0300

    Update sudoku.py

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -147,6 +147,10 @@ def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
 def check_solution(solution: tp.List[tp.List[str]]) -> bool:
     """Если решение solution верно, то вернуть True, в противном случае False"""
     # TODO: Add doctests with bad puzzles
+    for i in solution:
+        for j in i:
+            if j == ".":
+                return False
     n = len(solution)
     for i in range(n):
         t = set(get_row(solution, (i, 0))) | set(solution[i][0])
